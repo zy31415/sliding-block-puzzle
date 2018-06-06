4efc7d8f5e4c87743b862703445374ced8c04363
Yang Zhang <zy31415@gmail.com>
2018-06-06 20:40:44 +0800

    Implement interface.

--- src/main/java/io/github/zy31415/slidingblockpuzzle/solver/BreadthFirstSearch.java
+++ src/main/java/io/github/zy31415/slidingblockpuzzle/solver/BreadthFirstSearch.java
@@ -1,4 +1,8 @@
 package io.github.zy31415.slidingblockpuzzle.solver;
 
 public class BreadthFirstSearch implements Solver {
+    @Override
+    public Node search() {
+        return null;
+    }
 }
