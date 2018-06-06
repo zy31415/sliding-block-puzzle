5736df396bce85abf5467e87dad7468dc5e868dc
Yang Zhang <zy31415@gmail.com>
2018-06-06 20:44:51 +0800

    test github

--- src/main/java/io/github/zy31415/slidingblockpuzzle/solver/BreadthFirstSearch.java
+++ src/main/java/io/github/zy31415/slidingblockpuzzle/solver/BreadthFirstSearch.java
@@ -3,7 +3,7 @@ package io.github.zy31415.slidingblockpuzzle.solver;
 public class BreadthFirstSearch implements Solver {
     @Override
     public Node search() {
-        int b;
+        int c;
         return null;
     }
 }
